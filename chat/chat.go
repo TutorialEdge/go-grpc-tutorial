da7139199e45bdd0a0bcaedb6406bf80196fe5ec
Sanath Bhargav R <sanathbhargav26@gmail.com>
2020-11-26 11:30:07 +0530

    Update chat.go

--- chat/chat.go
+++ chat/chat.go
@@ -9,7 +9,7 @@ import (
 type Server struct {
 }
 
-func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
+func (s *Server) SayHello(ctx context.Context, message *Message) (*Message error) {
 	log.Printf("Received message body from client: %s", message.Body)
 	return &Message{Body: "Hello From the Server!"}, nil
 }
